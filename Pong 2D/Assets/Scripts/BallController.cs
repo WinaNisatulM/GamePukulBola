2632f74961c4c2c8efc2787bfd719c387f2586cb
WinaNisatulM <wnm.tp20@mail.mmtc.ac.id>
2021-09-03 17:48:06 +0700

    Pong 2D

---
+++ Pong 2D/Assets/Scripts/BallController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+   public int force;
+Rigidbody2D rigid;
+ // Use this for initialization
+ void Start () {
+ rigid = GetComponent<Rigidbody2D>();
+ Vector2 arah = new Vector2(2, 0).normalized;
+ rigid.AddForce(arah * force);
+ }
+ // Update is called once per frame
+ void Update () {
+ }
+ private void OnCollisionEnter2D(Collision2D coll)
+ {
+if (coll.gameObject.name == "TepiKanan")
+ {
+ ResetBall();
+ Vector2 arah = new Vector2(2, 0).normalized;
+ rigid.AddForce(arah * force);
+ }
+
+
+ if (coll.gameObject.name == "TepiKiri")
+ {
+ResetBall();
+ Vector2 arah = new Vector2(-2, 0).normalized;
+ rigid.AddForce(arah * force);
+ }
+ if (coll.gameObject.name == "Player1" || coll.gameObject.name == "Player2")
+ {
+ float sudut = (transform.position.y - coll.transform.position.y) * 5f;
+ Vector2 arah = new Vector2(rigid.velocity.x, sudut).normalized;
+ rigid.velocity = new Vector2(0, 0);
+ rigid.AddForce(arah * force * 2);
+ }
+ }
+ void ResetBall()
+ {
+ transform.localPosition = new Vector2(0, 0);
+ rigid.velocity = new Vector2(0, 0);
+ }
+ }
